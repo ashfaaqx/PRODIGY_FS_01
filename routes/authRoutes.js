c00a93755addc753966788b08fb7600d8a007b68
ashfaaqx <153409392+ashfaaqx@users.noreply.github.com>
2025-07-06 12:43:40 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,11 @@
+
 // Defines authentication-related routes: /register, /login, etc.
+// routes/authRoutes.js
+const express = require('express');
+const router = express.Router();
+const { registerUser } = require('../controllers/authController');
+
+// POST /api/auth/register
+router.post('/register', registerUser);
+
+module.exports = router;
